9ebcb71cc4cc1e47cdc126e6ea74c9977cfcad40
Axel Lord <axel.lord2000@gmail.com>
2025-04-13 14:08:58 +0200

    added async-fs-utils

---
+++ async-fs-utils-attr-impl/src/lib.rs
@@ -0,0 +1,339 @@
+//! Proc macro implementations.
+
+use ::proc_macro2::{Span, TokenStream};
+use ::quote::ToTokens;
+use ::syn::{
+    parse::{Parse, Parser},
+    parse2, parse_quote,
+    punctuated::Punctuated,
+    spanned::Spanned,
+    Attribute, Expr, ExprLit, FnArg, ItemFn, Lit, LitStr, Meta, MetaNameValue, Pat, PatIdent,
+    PatType, ReturnType, Stmt, Token, Type,
+};
+
+mod kw {
+    //! Custom keywords.
+
+    use ::syn::custom_keyword;
+
+    custom_keyword!(wrapped);
+    custom_keyword!(defer_err);
+    custom_keyword!(err);
+}
+
+/// Macro attributes.
+#[derive(Debug)]
+enum InBlockingAttr {
+    /// Wrapped blocking operation.
+    Wrapped {
+        /// Keyword.
+        wrapped: kw::wrapped,
+        /// Eq token.
+        eq_token: Token![=],
+        /// Path to blocking operation.
+        path: ::syn::Path,
+    },
+    /// Defer error information.
+    DeferErr {
+        /// Keyword.
+        defer_err: kw::defer_err,
+    },
+    /// Additional error information.
+    Err {
+        /// Keyword.
+        err: kw::err,
+        /// Eq token.
+        eq_token: Token![=],
+        /// Line to add to error section.
+        msg: LitStr,
+    },
+}
+
+impl ToTokens for InBlockingAttr {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        match self {
+            InBlockingAttr::Wrapped {
+                wrapped,
+                eq_token,
+                path,
+            } => {
+                wrapped.to_tokens(tokens);
+                eq_token.to_tokens(tokens);
+                path.to_tokens(tokens);
+            }
+            InBlockingAttr::DeferErr { defer_err } => defer_err.to_tokens(tokens),
+            InBlockingAttr::Err { err, eq_token, msg } => {
+                err.to_tokens(tokens);
+                eq_token.to_tokens(tokens);
+                msg.to_tokens(tokens);
+            }
+        }
+    }
+}
+
+impl Parse for InBlockingAttr {
+    fn parse(input: syn::parse::ParseStream) -> syn::Result<Self> {
+        let lookahead = input.lookahead1();
+        if lookahead.peek(kw::wrapped) {
+            Ok(InBlockingAttr::Wrapped {
+                wrapped: input.parse()?,
+                eq_token: input.parse()?,
+                path: input.parse()?,
+            })
+        } else if lookahead.peek(kw::defer_err) {
+            Ok(InBlockingAttr::DeferErr {
+                defer_err: input.parse()?,
+            })
+        } else if lookahead.peek(kw::err) {
+            Ok(InBlockingAttr::Err {
+                err: input.parse()?,
+                eq_token: input.parse()?,
+                msg: input.parse()?,
+            })
+        } else {
+            Err(lookahead.error())
+        }
+    }
+}
+
+/// If attribute is a doc attribute extract documentation.
+fn extract_doc(attr: &Attribute) -> Option<String> {
+    let doc: ::syn::Path = parse_quote!(doc);
+    if &doc != attr.path() {
+        return None;
+    }
+    let Meta::NameValue(MetaNameValue {
+        value: Expr::Lit(ExprLit {
+            lit: Lit::Str(lit_str),
+            ..
+        }),
+        ..
+    }) = &attr.meta
+    else {
+        return None;
+    };
+
+    Some(lit_str.value())
+}
+
+/// Attribute to put on functions that should be ran in a blocking thread.
+///
+/// # Errors
+/// Should the attribute be used on a non-function or if the function cannot be used.
+pub fn in_blocking(attr: TokenStream, item: TokenStream) -> ::syn::Result<TokenStream> {
+    let attr = Punctuated::<InBlockingAttr, Token![,]>::parse_terminated
+        .parse2(attr)?
+        .into_iter()
+        .collect();
+    let item = parse2(item)?;
+    in_blocking_(attr, item)
+}
+
+/// Typed implementation for [in_blocking].
+fn in_blocking_(attr: Vec<InBlockingAttr>, mut f: ItemFn) -> ::syn::Result<TokenStream> {
+    let path_attr_kw: ::syn::Path = parse_quote!(path);
+    let fd_attr_kw: ::syn::Path = parse_quote!(raw_fd);
+
+    let mut wrapped: Option<::syn::Path> = None;
+    let mut inner_doc = String::new();
+    let mut defer_err = false;
+    let mut extra_err = Vec::new();
+
+    for attr in &attr {
+        match attr {
+            InBlockingAttr::Wrapped { path, .. } => wrapped = Some(path.clone()),
+            InBlockingAttr::DeferErr { .. } => defer_err = true,
+            InBlockingAttr::Err { msg, .. } => extra_err.push(msg.value()),
+        }
+    }
+
+    f.attrs = f
+        .attrs
+        .into_iter()
+        .filter_map(|attr| {
+            if let Some(doc) = extract_doc(&attr) {
+                inner_doc.push_str(&doc);
+                None
+            } else {
+                Some(attr)
+            }
+        })
+        .collect();
+
+    let wrapped = wrapped.ok_or_else(|| {
+        ::syn::Error::new(
+            Span::call_site(),
+            format!(
+                "no wrapped attribute specified, specified attributes {:?}",
+                attr
+            ),
+        )
+    })?;
+
+    let mut args = Vec::<PatType>::new();
+    let mut arg_names = Vec::new();
+    let mut arg_docs = Vec::new();
+    let mut conversions = Vec::<Stmt>::new();
+    for arg in &mut f.sig.inputs {
+        let FnArg::Typed(arg) = arg else {
+            return Err(::syn::Error::new(
+                arg.span(),
+                "argument should not be a receiver",
+            ));
+        };
+
+        let mut is_path = false;
+        let mut is_fd = false;
+        let mut doc_attr = String::new();
+        arg.attrs = std::mem::take(&mut arg.attrs)
+            .into_iter()
+            .filter_map(|attr| {
+                if let Some(doc) = extract_doc(&attr) {
+                    doc_attr.push_str(&doc);
+                    Ok(None)
+                } else if &path_attr_kw == attr.path() {
+                    if matches!(attr.meta, Meta::Path(..)) {
+                        is_path = true;
+                        Ok(None)
+                    } else {
+                        Err(::syn::Error::new(
+                            attr.span(),
+                            "path attribute should not take any arguments",
+                        ))
+                    }
+                } else if &fd_attr_kw == attr.path() {
+                    if matches!(attr.meta, Meta::Path(..)) {
+                        is_fd = true;
+                        Ok(None)
+                    } else {
+                        Err(::syn::Error::new(
+                            attr.span(),
+                            "raw_fd attribute should not take any arguments",
+                        ))
+                    }
+                } else {
+                    Ok(Some(attr))
+                }
+                .transpose()
+            })
+            .collect::<Result<_, ::syn::Error>>()?;
+
+        let Pat::Ident(PatIdent { ident, .. }) = arg.pat.as_ref() else {
+            return Err(::syn::Error::new(
+                arg.pat.span(),
+                "all argument patterns should be identifiers",
+            ));
+        };
+
+        if !doc_attr.is_empty() {
+            arg_docs.push(format!("`{ident}`\n\n{doc_attr}"));
+        }
+
+        arg_names.push(ident.clone());
+
+        if is_path {
+            let ty = arg.ty.as_ref();
+            conversions.push(parse_quote!(let #ident: #ty = #ident.as_ref().into();));
+            args.push(parse_quote!(#ident: impl AsRef<::std::path::Path>));
+        } else if is_fd {
+            conversions.push(parse_quote!(let #ident: ::std::os::fd::RawFd = ::std::os::fd::IntoRawFd::into_raw_fd(#ident);));
+            args.push(parse_quote!(#ident: impl ::std::os::fd::IntoRawFd));
+        } else {
+            args.push(arg.clone());
+        }
+    }
+
+    let mut wrapped = wrapped.into_token_stream().to_string();
+    wrapped.retain(|c| !c.is_whitespace());
+
+    let name = f.sig.ident.clone();
+
+    let mut doc =
+        format!("Run [{wrapped}] in a blocking thread.\nSee also [try_{name}][self::try_{name}].");
+    let mut try_doc =
+        format!("Try to run [{wrapped}] in a blocking thread.\nSee also [{name}][self::{name}].");
+
+    if !inner_doc.is_empty() {
+        doc.push_str("\n\n");
+        doc.push_str(&inner_doc);
+
+        try_doc.push_str("\n\n");
+        try_doc.push_str(&inner_doc);
+    }
+
+    if !arg_docs.is_empty() {
+        doc.push_str("\n\n# Parameters");
+
+        try_doc.push_str("\n\n# Parameters");
+        for arg_doc in arg_docs {
+            doc.push_str("\n\n");
+            doc.push_str(&arg_doc);
+
+            try_doc.push_str("\n\n");
+            try_doc.push_str(&arg_doc);
+        }
+    }
+
+    try_doc
+        .push_str("\n\n# Errors\n\nIf the blocking task cannot be joined or has thrown a panic.");
+
+    if defer_err || !extra_err.is_empty() {
+        doc.push_str("\n\n# Errors");
+        inner_doc.push_str("\n\n# Errors");
+    }
+
+    for line in extra_err {
+        doc.push_str("\n\n");
+        doc.push_str(&line);
+
+        inner_doc.push_str("\n\n");
+        inner_doc.push_str(&line);
+    }
+
+    if defer_err {
+        doc.push_str(&format!("\n\nSee [{wrapped}]."));
+        inner_doc.push_str(&format!("\n\nSee [{wrapped}]."));
+        try_doc.push_str(&format!("\n\nFor inner see [{wrapped}]."));
+    }
+
+    doc.push_str("\n\n# Panics\nIf the blocking task cannot be joined or has thrown a panic.");
+
+    let doc = LitStr::new(&doc, Span::call_site());
+    let try_doc = LitStr::new(&try_doc, Span::call_site());
+    let ret_ty = f.sig.output.clone();
+
+    f.sig.ident = ::syn::Ident::new(&format!("__{}_blocking", f.sig.ident), f.sig.ident.span());
+    let inner_name = &f.sig.ident;
+
+    let ret_ty: Type = match ret_ty {
+        ReturnType::Default => parse_quote!(()),
+        ReturnType::Type(_, ty) => *ty,
+    };
+
+    let try_name = ::syn::Ident::new(&format!("try_{name}"), f.sig.ident.span());
+    let try_ret_ty: Type = parse_quote!(::std::result::Result<#ret_ty, ::tokio::task::JoinError>);
+
+    Ok(quote::quote!(
+        #[doc = #inner_doc]
+        #f
+        #[doc = #doc]
+        pub fn #name (#(#args),*) -> impl 'static + Send + ::std::future::Future<Output = #ret_ty> {
+            #(#conversions)*
+            async move {
+                unwrap_joined(
+                    ::tokio::task::spawn_blocking(move || {
+                        #inner_name(#(#arg_names),*)
+                    })
+                    .await
+                )
+            }
+        }
+        #[doc = #try_doc]
+        pub fn #try_name (#(#args),*) -> impl 'static + Send + ::std::future::Future<Output = #try_ret_ty> {
+            #(#conversions)*
+            ::tokio::task::spawn_blocking(move || {
+                #inner_name(#(#arg_names),*)
+            })
+        }
+    ))
+}
---
+++ async-fs-utils/src/lib.rs
@@ -0,0 +1,310 @@
+#![doc = include_str!("../../README.md")]
+
+use ::std::{
+    ffi::{OsStr, OsString},
+    future::Future,
+    io,
+    os::{
+        fd::{BorrowedFd, FromRawFd, OwnedFd, RawFd},
+        unix::ffi::OsStrExt,
+    },
+    panic::resume_unwind,
+    path::Path,
+};
+use std::{pin::Pin, task::Poll};
+
+use ::async_fs_utils_attr::in_blocking;
+use ::futures_core::FusedStream;
+use ::nix::{
+    dir::Dir,
+    errno::Errno,
+    fcntl::{AtFlags, OFlag, OpenHow, RenameFlags},
+    sys::stat::{FileStat, Mode},
+    NixPath,
+};
+use ::reflink_at::{OnExists, ReflinkAtError};
+use ::smallvec::SmallVec;
+use ::tokio::task::{JoinError, JoinHandle};
+use ::tokio_stream::Stream;
+
+/// Re-export of nix that matches version used by crate.
+pub use nix;
+
+/// SmallVec backed path, used for sending paths.
+#[derive(Debug)]
+struct OwnedPath(SmallVec<[u8; 32]>);
+
+impl OwnedPath {
+    /// Create a new owned path from a path-like.
+    pub fn new<P: AsRef<Path>>(path: P) -> Self {
+        Self(path.as_ref().as_os_str().as_bytes().into())
+    }
+}
+
+impl NixPath for OwnedPath {
+    fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    fn len(&self) -> usize {
+        self.0.len()
+    }
+
+    fn with_nix_path<T, F>(&self, f: F) -> nix::Result<T>
+    where
+        F: FnOnce(&std::ffi::CStr) -> T,
+    {
+        OsStr::from_bytes(&self.0).with_nix_path(f)
+    }
+}
+
+impl AsRef<Path> for OwnedPath {
+    fn as_ref(&self) -> &Path {
+        Path::new(OsStr::from_bytes(&self.0))
+    }
+}
+
+impl From<&Path> for OwnedPath {
+    fn from(value: &Path) -> Self {
+        Self::new(value)
+    }
+}
+
+/// Unwrap a join result by either panic or repanic.
+///
+/// # Panics
+/// If the thread did panic or was canceled.
+fn unwrap_joined<T>(t: Result<T, JoinError>) -> T {
+    let err = match t {
+        Ok(t) => return t,
+        Err(err) => err,
+    };
+
+    match err.try_into_panic() {
+        Ok(p) => resume_unwind(p),
+        Err(err) => panic!("blocking thread canceled, {err}"),
+    }
+}
+
+/// Read a directory as a stream.
+pub fn read_dir(dir: Dir) -> impl Send + FusedStream<Item = Result<nix::dir::Entry, Errno>> {
+    /// Read dir stream.
+    #[derive(Debug)]
+    struct ReadDir(Option<JoinHandle<Option<IterEntry>>>);
+
+    /// Iter entry pair.
+    #[derive(Debug)]
+    struct IterEntry(nix::dir::OwningIter, Result<nix::dir::Entry, Errno>);
+
+    impl Stream for ReadDir {
+        type Item = Result<nix::dir::Entry, Errno>;
+
+        fn poll_next(
+            mut self: Pin<&mut Self>,
+            cx: &mut std::task::Context<'_>,
+        ) -> Poll<Option<Self::Item>> {
+            // If no JoinHandle exists stream has finished.
+            let Some(mut handle) = self.0.take() else {
+                return Poll::Ready(None);
+            };
+
+            // Only case that returns pending waking handled by poll of JoinHandle.
+            let Poll::Ready(value) = Pin::new(&mut handle).poll(cx) else {
+                self.0 = Some(handle);
+                return Poll::Pending;
+            };
+
+            // Handle returned finished stream. Might panic Might panic.
+            let Some(IterEntry(mut iter, entry)) = unwrap_joined(value) else {
+                return Poll::Ready(None);
+            };
+
+            // Get next entry.
+            self.0 = Some(tokio::task::spawn_blocking(move || {
+                iter.next().map(|next| IterEntry(iter, next))
+            }));
+
+            Poll::Ready(Some(entry))
+        }
+    }
+
+    impl FusedStream for ReadDir {
+        fn is_terminated(&self) -> bool {
+            self.0.is_none()
+        }
+    }
+
+    ReadDir(Some(tokio::task::spawn_blocking(move || {
+        let mut iter = dir.into_iter();
+        iter.next().map(|next| IterEntry(iter, next))
+    })))
+}
+
+#[in_blocking(wrapped = nix::sys::stat::fstat, defer_err)]
+fn file_stat(
+    /// file to stat
+    fd: RawFd,
+) -> Result<FileStat, Errno> {
+    nix::sys::stat::fstat(fd)
+}
+
+#[in_blocking(wrapped = nix::sys::stat::fstatat, defer_err)]
+fn file_stat_at(
+    /// Directory to resolve path in.
+    fd: Option<RawFd>,
+    /// File to stat.
+    #[path]
+    path: OwnedPath,
+    /// Flags used when resolving path.
+    at_flags: AtFlags,
+) -> Result<FileStat, Errno> {
+    nix::sys::stat::fstatat(fd, &path, at_flags)
+}
+
+#[in_blocking(wrapped = nix::dir::Dir::from, defer_err)]
+fn open_dir(
+    /// Directory to open. Responsibility to close it is transferred to returned object.
+    #[raw_fd]
+    fd: RawFd,
+) -> Result<Dir, Errno> {
+    Dir::from_fd(fd)
+}
+
+#[in_blocking(wrapped = OwnedFd::try_clone, defer_err)]
+fn clone_fd(
+    /// File descriptor to clone.
+    fd: OwnedFd,
+) -> (OwnedFd, Result<OwnedFd, io::Error>) {
+    let cloned = fd.try_clone();
+    (fd, cloned)
+}
+
+#[in_blocking(wrapped = nix::fcntl::openat2, defer_err)]
+fn open_at_2(
+    /// Direcory to resolve path in.
+    dir_fd: RawFd,
+    /// File to open.
+    #[path]
+    path: OwnedPath,
+    /// How to open file, such as how paths are resolved and what mode new files use.
+    how: OpenHow,
+) -> Result<OwnedFd, Errno> {
+    nix::fcntl::openat2(dir_fd, &path, how).map(|fd| unsafe { OwnedFd::from_raw_fd(fd) })
+}
+
+#[in_blocking(wrapped = nix::fcntl::openat, defer_err)]
+fn open_at(
+    /// Directory to resolve path in.
+    dir_fd: Option<RawFd>,
+    /// File to open.
+    #[path]
+    path: OwnedPath,
+    /// What mode to use if creating the file.
+    mode: Mode,
+    /// File open flags, such as O_RDWR for read-write or O_RDONLY for read-only.
+    flags: OFlag,
+) -> Result<OwnedFd, Errno> {
+    nix::fcntl::openat(dir_fd, &path, flags, mode).map(|fd| unsafe { OwnedFd::from_raw_fd(fd) })
+}
+
+#[in_blocking(wrapped = nix::fcntl::readlinkat, defer_err)]
+fn read_link_at(
+    /// Directory to resolve path in.
+    dir_fd: Option<RawFd>,
+    /// Path to link to read.
+    #[path]
+    path: OwnedPath,
+) -> Result<OsString, Errno> {
+    nix::fcntl::readlinkat(dir_fd, &path)
+}
+
+/// ´dir_fd´ and ´dest´ specify on what filesystem to create the reflink.
+#[in_blocking(wrapped = reflink_at::reflink_unlinked, defer_err)]
+fn reflink_unlinked(
+    /// Directory to resolve dest in.
+    dir_fd: Option<RawFd>,
+    /// Where to create reflink.
+    #[path]
+    dest: OwnedPath,
+    /// File to reflik to.
+    src: RawFd,
+    /// What file mode to use for created file.
+    mode: Mode,
+) -> Result<OwnedFd, Errno> {
+    reflink_at::reflink_unlinked(
+        dir_fd.map(|dir_fd| unsafe { BorrowedFd::borrow_raw(dir_fd) }),
+        dest.as_ref(),
+        unsafe { BorrowedFd::borrow_raw(src) },
+        mode,
+    )
+}
+
+#[in_blocking(wrapped = reflink_at::reflink, defer_err)]
+fn reflink(
+    /// File to overwrite with reflink.
+    dest: RawFd,
+    /// File to reflink to.
+    src: RawFd,
+) -> Result<(), Errno> {
+    reflink_at::reflink(unsafe { BorrowedFd::borrow_raw(dest) }, unsafe {
+        BorrowedFd::borrow_raw(src)
+    })
+}
+
+#[in_blocking(wrapped = reflink_at::reflink_at, defer_err)]
+fn reflink_at(
+    /// Directory file descriptor to resolve dest in.
+    dir_fd: Option<RawFd>,
+    /// Where to create reflink.
+    #[path]
+    dest: OwnedPath,
+    /// File to reflink to.
+    src: RawFd,
+    /// What file mode to use for created reflink.
+    mode: Mode,
+    /// How to handle a file existing at dest.
+    on_exists: OnExists,
+) -> Result<OwnedFd, ReflinkAtError> {
+    reflink_at::reflink_at(
+        dir_fd.map(|dir_fd| unsafe { BorrowedFd::borrow_raw(dir_fd) }),
+        dest.as_ref(),
+        unsafe { BorrowedFd::borrow_raw(src) },
+        mode,
+        on_exists,
+    )
+}
+
+#[in_blocking(wrapped = nix::fcntl::renameat, defer_err)]
+fn rename_at(
+    /// Directory to resolve old_path in.
+    old_dir_fd: Option<RawFd>,
+    /// File to rename.
+    #[path]
+    old_path: OwnedPath,
+    /// Directory to resolve new_path in.
+    new_dir_fd: Option<RawFd>,
+    /// What to rename file to.
+    #[path]
+    new_path: OwnedPath,
+) -> Result<(), Errno> {
+    nix::fcntl::renameat(old_dir_fd, &old_path, new_dir_fd, &new_path)
+}
+
+#[in_blocking(wrapped = nix::fcntl::renameat2, defer_err)]
+fn rename_at_2(
+    /// Directory to resolve old_path in.
+    old_dir_fd: Option<RawFd>,
+    /// File to rename.
+    #[path]
+    old_path: OwnedPath,
+    /// Directory to resolve new_path in.
+    new_dir_fd: Option<RawFd>,
+    /// What to rename file to.
+    #[path]
+    new_path: OwnedPath,
+    /// Additional flags to use when renaming, such as whether or not to replace existing files or
+    /// swap with them.
+    flags: RenameFlags,
+) -> Result<(), Errno> {
+    nix::fcntl::renameat2(old_dir_fd, &old_path, new_dir_fd, &new_path, flags)
+}
