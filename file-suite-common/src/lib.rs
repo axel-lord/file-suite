634b9ca8f1309e852af91fb4823e1953b65611f7
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2025-04-19 23:22:31 +0200

    rewrote Startable somewhat

--- file-suite-common/src/lib.rs
+++ file-suite-common/src/lib.rs
@@ -5,7 +5,7 @@ use ::std::process::ExitCode;
 use ::clap::{CommandFactory, FromArgMatches};
 use ::color_eyre::Report;
 
-pub use crate::{run::Run, start::Start, startable::Startable};
+pub use crate::{run::Run, start::Start, startable::startable};
 
 mod run;
 mod start;
@@ -31,15 +31,15 @@ impl From<u8> for ExitCodeError {
     }
 }
 
-/// Invoke [Start::start] for T as if it using [Startable].
+/// Invoke [Start::start] for T as if it using [startable].
 ///
 /// # Errors
 /// If panic handler cannot be installed or the [Run::run] implementation needs to.
 pub fn start<T>(modules: &[&str]) -> Result
 where
-    T: Run + CommandFactory + FromArgMatches,
+    T: Run + CommandFactory + FromArgMatches + 'static,
     T::Err: Send + Sync,
     Report: From<T::Err>,
 {
-    Startable::<T>::new().start(modules)
+    startable::<T>().start(modules)
 }
