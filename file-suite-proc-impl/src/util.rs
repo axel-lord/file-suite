051d2fad38f38e58909897f1b07ebf12659fc3a6
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2025-05-01 21:35:37 +0200

    refined parsing code and added type defaults for kebab expression

--- file-suite-proc-impl/src/util.rs
+++ file-suite-proc-impl/src/util.rs
@@ -1,5 +1,7 @@
 //! Proc macro utilities.
 
+use ::std::iter;
+
 pub(crate) use self::{
     any_of::{AnyOf3, Either},
     kw_kind::kw_kind,
@@ -13,3 +15,15 @@ mod lookahead;
 mod kw_kind;
 
 pub mod tcmp;
+
+/// Create an iterator that repeats a function result n times then yields the result of the
+/// termination function.
+pub(crate) fn do_n_times_then<T>(
+    n: usize,
+    repeater: impl FnMut() -> T,
+    term: impl FnOnce() -> T,
+) -> impl Iterator<Item = T> {
+    iter::repeat_with(repeater)
+        .take(n)
+        .chain(iter::once_with(term))
+}
