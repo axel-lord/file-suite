23671c5691bd954920481ec0e7859ba1611b4886
Axel Lord <axel.lord2000@gmail.com>
2025-05-03 01:22:17 +0200

    moved count from split to combine

--- file-suite-proc-impl/src/kebab/output.rs
+++ file-suite-proc-impl/src/kebab/output.rs
@@ -163,23 +163,27 @@ kw_kind!(
         Snake snake,
         /// Values should be joined by a space.
         Space space,
+        /// Values should be counted.
+        Count count,
     }
 );
 
 impl CombineKind {
     /// Join input arguments.
     pub fn join(self, values: Vec<String>) -> String {
-        values.join(match self {
-            CombineKind::Concat => "",
-            CombineKind::Kebab => "-",
-            CombineKind::Snake => "_",
-            CombineKind::Space => " ",
-        })
+        match self {
+            CombineKind::Concat => values.join(""),
+            CombineKind::Kebab => values.join("-"),
+            CombineKind::Snake => values.join("_"),
+            CombineKind::Space => values.join(" "),
+            CombineKind::Count => values.len().to_string(),
+        }
     }
 
     /// Preferred [TyKind] of variant.
     pub const fn default_ty(self) -> Option<TyKind> {
         Some(match self {
+            Self::Count => TyKind::LitInt,
             Self::Space | Self::Kebab => TyKind::LitStr,
             _ => return None,
         })
