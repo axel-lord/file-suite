8ca7756ed8b518949c2480137caef8a86b232cb1
Axel Lord <axel.lord2000@gmail.com>
2025-05-18 01:43:30 +0200

    in file-suite-proc, added exports of array_expr and array_expr_paste proc macros from file-suite-proc-impl

--- file-suite-proc/src/lib.rs
+++ file-suite-proc/src/lib.rs
@@ -21,6 +21,22 @@ pub fn kebab_paste(input: TokenStream) -> TokenStream {
         .into()
 }
 
+/// Find array expressions in input token and replace them with computed result.
+#[proc_macro]
+pub fn array_expr_paste(input: TokenStream) -> TokenStream {
+    ::file_suite_proc_impl::array_expr_paste(input.into())
+        .unwrap_or_else(|err| err.into_compile_error())
+        .into()
+}
+
+/// Calculate an array expression on input.
+#[proc_macro]
+pub fn array_expr(input: TokenStream) -> TokenStream {
+    ::file_suite_proc_impl::array_expr(input.into())
+        .unwrap_or_else(|err| err.into_compile_error())
+        .into()
+}
+
 #[expect(missing_docs)]
 #[proc_macro_derive(Run, attributes(run))]
 pub fn derive_run(input: TokenStream) -> TokenStream {
