ccfd4cb5a6da9e317b9f0f16665b9d097c3bcfa7
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2025-09-25 20:58:24 +0200

    in common-prefix, added pattern value parser

--- tools/common-prefix/src/lib.rs
+++ tools/common-prefix/src/lib.rs
@@ -1,8 +1,16 @@
 #![doc = include_str!("../README.md")]
 
-use ::std::io::{Read, Write};
+use ::std::{
+    cmp::Ordering,
+    io::{Read, Write},
+};
 
 use ::itertools::{Itertools, MinMaxResult};
+use ::regex::bytes::{Regex, RegexBuilder};
+
+fn compile_re(pattern: &str) -> Result<Regex, ::regex::Error> {
+    RegexBuilder::new(pattern).multi_line(true).build()
+}
 
 /// Find the common prefix of all lines piped to stdin.
 ///
@@ -20,8 +28,16 @@ pub struct Cli {
 
     /// Use component equality with components split by a value
     /// instead of byte equality.
-    #[arg(long, short, default_missing_value = "/", require_equals = true, num_args = 0..=1, value_name = "DELIM")]
-    components: Option<String>,
+    #[arg(
+        long,
+        short,
+        default_missing_value = "/",
+        value_parser = compile_re,
+        require_equals = true,
+        num_args = 0..=1,
+        value_name = "DELIM_PATTERN"
+    )]
+    components: Option<Regex>,
 }
 
 fn by_byte_prefix(items: MinMaxResult<&[u8]>) -> ::std::io::Result<()> {
@@ -46,6 +62,50 @@ fn by_byte_prefix(items: MinMaxResult<&[u8]>) -> ::std::io::Result<()> {
             ::std::io::stdout().lock().write_all(prefix)
         }
     }
+}
+
+#[derive(Debug, Clone, Copy, Hash, PartialEq, Eq, PartialOrd, Ord)]
+struct Component<'b> {
+    pub value: &'b [u8],
+    pub sep: Option<&'b [u8]>,
+}
+
+impl<'b> Component<'b> {
+    fn parse(re: &Regex, bytes: &'b [u8], at: usize) -> (Self, usize) {
+        if let Some(mat) = re.find_at(bytes, at) {
+            let value = &bytes[at..mat.start()];
+            let sep = Some(mat.as_bytes());
+            (Self { value, sep }, mat.end())
+        } else {
+            let value = &bytes[at..];
+            let sep = None;
+            (Self { value, sep }, bytes.len())
+        }
+    }
+
+    fn parse_all(re: &'b Regex, bytes: &'b [u8]) -> impl 'b + Iterator<Item = Self> {
+        let mut at = 0;
+        ::std::iter::from_fn(move || {
+            if at >= bytes.len() {
+                return None;
+            }
+            let comp;
+            (comp, at) = Self::parse(re, bytes, at);
+            Some(comp)
+        })
+    }
+
+    fn cmp_multiple(a: &[Self], b: &[Self]) -> Ordering {
+        let a = a.iter().map(|comp| comp.value);
+        let b = b.iter().map(|comp| comp.value);
+        ::std::iter::Iterator::cmp(a, b)
+    }
+}
+
+fn by_component<'a>(items: impl Iterator<Item = &'a [u8]>, re: Regex) {
+    let items = items
+        .map(|item| Component::parse_all(&re, item).collect::<Vec<_>>())
+        .minmax_by(|a, b| Component::cmp_multiple(a, b));
 }
 
 impl ::file_suite_common::Run for Cli {
