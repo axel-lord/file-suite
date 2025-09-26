7e917195a7a4ec8d6e7cf10f8c546df1ff8e8ab5
Axel Lord <76226429+axel-lord@users.noreply.github.com>
2025-09-26 16:30:43 +0200

    in common-prefix, added functionality of component splitting, and pair printing

--- tools/common-prefix/src/lib.rs
+++ tools/common-prefix/src/lib.rs
@@ -18,12 +18,7 @@ fn compile_re(pattern: &str) -> Result<Regex, ::regex::Error> {
 #[derive(Debug, ::clap::Parser)]
 pub struct Cli {
     /// Input is separated by null characters.
-    #[arg(
-        short = '0',
-        long = "null",
-        visible_alias = "print0",
-        visible_short_alias = 'z'
-    )]
+    #[arg(short = '0', long, visible_alias = "print0", visible_short_alias = 'z')]
     null: bool,
 
     /// Use component equality with components split by a value
@@ -38,12 +33,17 @@ pub struct Cli {
         value_name = "DELIM_PATTERN"
     )]
     components: Option<Regex>,
+
+    /// After finding the prefix, print all non-empty input lines and them without the prefix
+    /// separated by newlines or null bytes depending on the 'null' option.
+    #[arg(long, visible_alias = "pairs")]
+    print_pairs: bool,
 }
 
-fn by_byte_prefix(items: MinMaxResult<&[u8]>) -> ::std::io::Result<()> {
+fn by_byte_prefix(items: MinMaxResult<&[u8]>) -> ::std::io::Result<&[u8]> {
     match items {
         MinMaxResult::NoElements => Err(::std::io::Error::other("input is empty")),
-        MinMaxResult::OneElement(line) => ::std::io::stdout().lock().write_all(line),
+        MinMaxResult::OneElement(line) => Ok(line),
         MinMaxResult::MinMax(first, last) => {
             let prefix = {
                 let mut prefix = b"".as_slice();
@@ -59,7 +59,7 @@ fn by_byte_prefix(items: MinMaxResult<&[u8]>) -> ::std::io::Result<()> {
                 prefix
             };
 
-            ::std::io::stdout().lock().write_all(prefix)
+            Ok(prefix)
         }
     }
 }
@@ -68,6 +68,12 @@ fn by_byte_prefix(items: MinMaxResult<&[u8]>) -> ::std::io::Result<()> {
 struct Component<'b> {
     pub value: &'b [u8],
     pub sep: Option<&'b [u8]>,
+}
+
+impl<'b> AsRef<Component<'b>> for Component<'b> {
+    fn as_ref(&self) -> &Component<'b> {
+        self
+    }
 }
 
 impl<'b> Component<'b> {
@@ -95,6 +101,19 @@ impl<'b> Component<'b> {
         })
     }
 
+    fn write_all(
+        comps: impl IntoIterator<Item = impl AsRef<Self>>,
+        mut w: impl Write,
+    ) -> ::std::io::Result<()> {
+        for comp in comps {
+            w.write_all(comp.as_ref().value)?;
+            if let Some(sep) = comp.as_ref().sep {
+                w.write_all(sep)?;
+            }
+        }
+        Ok(())
+    }
+
     fn cmp_multiple(a: &[Self], b: &[Self]) -> Ordering {
         let a = a.iter().map(|comp| comp.value);
         let b = b.iter().map(|comp| comp.value);
@@ -102,10 +121,38 @@ impl<'b> Component<'b> {
     }
 }
 
-fn by_component<'a>(items: impl Iterator<Item = &'a [u8]>, re: Regex) {
+fn by_component<'a>(
+    items: impl IntoIterator<Item = impl AsRef<[Component<'a>]>>,
+    w: impl Write,
+) -> ::std::io::Result<usize> {
     let items = items
-        .map(|item| Component::parse_all(&re, item).collect::<Vec<_>>())
-        .minmax_by(|a, b| Component::cmp_multiple(a, b));
+        .into_iter()
+        .minmax_by(|a, b| Component::cmp_multiple(a.as_ref(), b.as_ref()));
+
+    match items {
+        MinMaxResult::NoElements => Err(::std::io::Error::other("input is empty")),
+        MinMaxResult::OneElement(elem) => {
+            Component::write_all(elem.as_ref(), w)?;
+            Ok(elem.as_ref().len())
+        }
+        MinMaxResult::MinMax(first, last) => {
+            let prefix = {
+                let mut prefix: &[Component] = &[];
+                for i in 0.. {
+                    let Some(initial) = first.as_ref().get(0..i) else {
+                        break;
+                    };
+                    if !last.as_ref().starts_with(initial) {
+                        break;
+                    }
+                    prefix = initial;
+                }
+                prefix
+            };
+            Component::write_all(prefix, w)?;
+            Ok(prefix.as_ref().len())
+        }
+    }
 }
 
 impl ::file_suite_common::Run for Cli {
@@ -119,12 +166,47 @@ impl ::file_suite_common::Run for Cli {
         };
 
         let delim = if self.null { b'\0' } else { b'\n' };
-        let items = bytes.split(|e| *e == delim);
+        let items = bytes.split(|e| *e == delim).filter(|line| !line.is_empty());
 
-        if let Some(_pat) = self.components {
-            todo!()
+        if let Some(pat) = self.components {
+            let items = items.map(|bytes| Component::parse_all(&pat, bytes).collect::<Vec<_>>());
+
+            if self.print_pairs {
+                let items = items.collect::<Vec<_>>();
+                let mut stdout = ::std::io::stdout().lock();
+                let len = by_component(&items, &mut stdout)?;
+
+                for item in items {
+                    stdout.write_all(&[delim])?;
+                    Component::write_all(&item, &mut stdout)?;
+                    stdout.write_all(&[delim])?;
+                    Component::write_all(&item[len..], &mut stdout)?;
+                }
+
+                Ok(())
+            } else {
+                by_component(items, ::std::io::stdout().lock())?;
+                Ok(())
+            }
         } else {
-            by_byte_prefix(items.filter(|line| !line.is_empty()).minmax())
+            if self.print_pairs {
+                let items = items.collect::<Vec<_>>();
+                let prefix = by_byte_prefix(items.iter().copied().minmax())?;
+                let len = prefix.len();
+
+                let mut stdout = ::std::io::stdout().lock();
+                stdout.write_all(prefix)?;
+
+                for item in items {
+                    stdout.write_all(&[delim])?;
+                    stdout.write_all(&item[len..])?;
+                }
+
+                Ok(())
+            } else {
+                let prefix = by_byte_prefix(items.minmax())?;
+                ::std::io::stdout().lock().write_all(prefix)
+            }
         }
     }
 }
